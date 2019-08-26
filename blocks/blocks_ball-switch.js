1c401b3906d822c0f7316ad4a591bd6097ef41f4
Prawared Bowonphattharawadi <prawared.bw@gmail.com>
2019-08-26 16:14:48 +0700

    add generator

--- blocks/blocks_ball-switch.js
+++ blocks/blocks_ball-switch.js
@@ -1,15 +1,18 @@
-Blockly.Blocks['ky_020_tilt_switch_block'] = {
-  init: function() {
-    this.appendDummyInput()
-        .appendField("KY-020 TILT SWITCH");
-    this.appendValueInput("KY_020_TILT_SWITCH_PIN")
-        .setCheck("Number")
-        .appendField("PIN");
-    this.setInputsInline(true);
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
-    this.setColour(230);
- this.setTooltip("");
- this.setHelpUrl("");
-  }
-};
+Blockly.Blocks["ky_020_tilt_switch_block"] = {
+    init: function() {
+      this.appendDummyInput().appendField("KY-020 TILT SWITCH");
+      this.appendDummyInput()
+        .appendField("PIN")
+        .appendField(new Blockly.FieldTextInput("0"), "KY_020_TILT_SWITCH_PIN")
+        .appendField("SET TO")
+        .appendField(
+          new Blockly.FieldVariable("KY_020_TILT_SWITCH_VALUE"),
+          "KY_020_TILT_SWITCH_VALUE"
+        );
+      this.setPreviousStatement(true, null);
+      this.setNextStatement(true, null);
+      this.setColour(230);
+      this.setTooltip("");
+      this.setHelpUrl("");
+    }
+  };
--- blocks/generators_ball-switch.js
+++ blocks/generators_ball-switch.js
@@ -1,6 +1,24 @@
-Blockly.JavaScript['ky_020_tilt_switch_block'] = function(block) {
-  var value_ky_020_tilt_switch_pin = Blockly.JavaScript.valueToCode(block, 'KY_020_TILT_SWITCH_PIN', Blockly.JavaScript.ORDER_ATOMIC);
-  // TODO: Assemble JavaScript into code variable.
-  var code = '...;\n';
-  return code;
-};
+Blockly.JavaScript["ky_020_tilt_switch_block"] = function(block) {
+    var text_ky_020_tilt_switch_pin = block.getFieldValue(
+      "KY_020_TILT_SWITCH_PIN"
+    );
+    var variable_ky_020_tilt_switch_value = Blockly.JavaScript.variableDB_.getName(
+      block.getFieldValue("KY_020_TILT_SWITCH_VALUE"),
+      Blockly.Variables.NAME_TYPE
+    );
+    // TODO: Assemble JavaScript into code variable.
+    var code = `
+    #VARIABLE
+	int KY_020_TILT_SWITCH_PIN = ${text_ky_020_tilt_switch_pin};      // pin number for tilt switch signal
+	int KY_020_TILT_SWITCH_STATE = 0;    // variable for reading the tilt switch status
+	#END
+
+	#SETUP
+	  pinMode(KY_020_TILT_SWITCH_PIN, INPUT);  // set the tilt switch pin as input
+	#END
+
+	  // get the tilt switch state
+	  KY_020_TILT_SWITCH_STATE = digitalRead(KY_020_TILT_SWITCH_PIN);
+    `;
+    return code;
+  };
